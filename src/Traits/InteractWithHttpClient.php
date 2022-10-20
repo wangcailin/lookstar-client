a7a0d81d977007aecfcaad02a5d5953e24e7bedf
王彩霖 <root@Bluedot-Development-Cailin.localdomain>
2022-10-20 11:11:59 +0800

    feat(response): 增加转义数组

--- src/Traits/InteractWithHttpClient.php
+++ src/Traits/InteractWithHttpClient.php
@@ -58,7 +58,7 @@ trait InteractWithHttpClient
         $method = strtoupper($method);
 
         $response = $this->getHttpClient()->request($method, 'https://' . $this->domain . $this->prefix . $url, $options);
-        return $response;
+        return json_decode($response->getBody()->getContents());
     }
 
     private function getHttpClient()
